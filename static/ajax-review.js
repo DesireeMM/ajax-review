486f2e792248fcf15d85b7df0a58411ceddc7051
DesireeMM <dmorimoto89@gmail.com>
2023-04-06 17:45:04 -0700

    Finished HW

---
+++ static/ajax-review.js
@@ -0,0 +1,27 @@
+// PART 1: SHOW A FORTUNE
+
+function showFortune() {
+  fetch('/fortune')
+    .then((response) => response.text())
+    .then((responseData) => {
+      document.querySelector('#fortune-text').innerHTML = responseData;
+  })
+}
+
+document.querySelector('#get-fortune-button').addEventListener('click', showFortune);
+
+// PART 2: SHOW WEATHER
+
+function showWeather(evt) {
+  evt.preventDefault();
+  const zipcode = document.querySelector('#zipcode-field').value;
+  const url = `/weather?zipcode=${zipcode}`;
+  
+  fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    document.querySelector('#weather-info').innerText = data.forecast;
+  });
+}
+
+document.querySelector('#weather-form').addEventListener('submit', showWeather);
